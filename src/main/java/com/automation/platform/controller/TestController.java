fe40e6a608bbb44e9809ee2af94acdcd4d484acb
cindyfeng1987@163.com <cindyfeng1987@163.com>
2021-04-13 11:20:38 +0800

    添加热部署测试和value scan 配置值

--- src/main/java/com/automation/platform/controller/TestController.java
+++ src/main/java/com/automation/platform/controller/TestController.java
@@ -1,13 +1,23 @@
 package com.automation.platform.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    @Value("${test.hello}")
+    public String hello;
+
     @GetMapping("/hello")
-    public String hello(){
-        return "Hello World!";
+    public String hello() {
+        return "Hello World!\n"+ hello;
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost (String name){
+        return "Hello World! Post" + name;
     }
 }
