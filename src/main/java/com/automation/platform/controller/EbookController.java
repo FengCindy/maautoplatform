7cacc5a86383a2b7f140dd4cd6301bd19bd53588
cindyfeng1987@163.com <cindyfeng1987@163.com>
2021-04-25 10:51:00 +0800

    增加CommonResp通用类

--- src/main/java/com/automation/platform/controller/EbookController.java
+++ src/main/java/com/automation/platform/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.automation.platform.controller;
 
 import com.automation.platform.domain.Ebook;
+import com.automation.platform.resp.CommonResp;
 import com.automation.platform.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +19,11 @@ public class EbookController {
 
 
     @GetMapping("/list")
-    public List<Ebook> list() {
-        return ebookService.list();
+    public CommonResp  list()
+    {
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 }
--- src/main/java/com/automation/platform/mapper/TestMapper.java
+++ src/main/java/com/automation/platform/mapper/TestMapper.java
@@ -2,6 +2,11 @@ package com.automation.platform.mapper;
 
 import com.automation.platform.domain.Test;
 
+
+
+/**
+天津
+ */
 import java.util.List;
 
 public interface TestMapper {
