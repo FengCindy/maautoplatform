4b0f3275fa44e39a419be805163babcf960c67d6
cindyfeng1987@163.com <cindyfeng1987@163.com>
2021-04-30 14:04:30 +0800

    使用ref和reactive实现数据绑定

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -48,27 +48,39 @@
     <a-layout-content
             :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      Content
+        <pre>
+          {{ebooks}}
+          {{ebooks2}}
+        </pre>
     </a-layout-content>
   </a-layout>
 </template>
 
 <script lang="ts">
-import { defineComponent,onMounted } from 'vue';
+import { defineComponent,onMounted,ref,reactive,toRef} from 'vue';
 import axios from 'axios';
 
 export default defineComponent({
   name: 'Home',
   setup(){
     console.log("setup");
+    const ebooks = ref();
+    const ebooks1 = reactive({books : []});
 
     onMounted(() =>{
       console.log("onMounted");
       axios.get("http://localhost:8090/ebook/list?name=Spring").then((response) =>{
-        // data = response.data
+        const data = response.data;
+        ebooks.value = data.content;
+        ebooks1.books = data.content;
         console.log(response);
     })
-  })
+  });
+
+    return{
+      ebooks,
+      ebooks2: toRef(ebooks1,"books")
+    }
   }
 });
 </script>
