74df372866b3d84a4f8d432fc3bc8c1e9eaeac2e
cindyfeng1987@163.com <cindyfeng1987@163.com>
2021-05-27 15:05:13 +0800

    电子书管理页面前后端分页整合完成 前端打印接口请求和返回日志

--- web/src/main.ts
+++ web/src/main.ts
@@ -12,6 +12,16 @@ axios.defaults.baseURL=process.env.VUE_APP_SERVER;
 /**
  *
  */
+axios.interceptors.request.use(function (config) {
+    console.log('请求参数：', config);
+    return config;
+},error => {
+    return Promise.reject(error);
+});
+axios.interceptors.response.use(function (response) {
+    console.log('返回结果：', response);
+    return response;
+})
 
 const  app = createApp(App);
 app.use(store).use(router).use(Antd).mount('#app');
